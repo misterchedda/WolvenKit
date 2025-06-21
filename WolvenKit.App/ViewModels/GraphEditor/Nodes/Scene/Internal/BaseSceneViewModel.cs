dd0f02142c6c69aac206f78e0be1cbd962781487
MisterChedda <mister.chedda.here@gmail.com>
2025-06-22 03:36:35 +0530

    feat: add colors to GraphEditor

--- WolvenKit.App/ViewModels/GraphEditor/Nodes/Scene/Internal/BaseSceneViewModel.cs
+++ WolvenKit.App/ViewModels/GraphEditor/Nodes/Scene/Internal/BaseSceneViewModel.cs
@@ -1,4 +1,5 @@
-﻿using WolvenKit.RED4.Types;
+﻿using System.Windows.Media;
+using WolvenKit.RED4.Types;
 
 namespace WolvenKit.App.ViewModels.GraphEditor.Nodes.Scene.Internal;
 
@@ -6,9 +7,30 @@ public abstract class BaseSceneViewModel : NodeViewModel
 {
     public override uint UniqueId => ((scnSceneGraphNode)Data).NodeId.Id;
 
+    public Brush Background { get; protected set; }
+
     protected BaseSceneViewModel(scnSceneGraphNode scnSceneGraphNode) : base(scnSceneGraphNode)
     {
         Title = $"[{UniqueId}] {Data.GetType().Name[3..^4]}";
+        Background = GetBackgroundForNodeType(scnSceneGraphNode);
+    }
+
+    private static Brush GetBackgroundForNodeType(scnSceneGraphNode node)
+    {
+        // Create a slight dark tint background based on node type
+        return node switch
+        {
+            scnStartNode => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#33228B22")), // Dark green tint
+            scnEndNode => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#33B22222")), // Dark red tint
+            scnChoiceNode => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#332288B2")), // Dark blue tint
+            scnQuestNode => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#33B2B222")), // Dark yellow tint
+            scnSectionNode => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#33B222B2")), // Dark purple tint
+            scnRandomizerNode => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3322B2B2")), // Dark cyan tint
+            scnHubNode => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#33B2AA22")), // Dark orange tint
+            scnAndNode => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#3355AA22")), // Dark lime tint
+            scnXorNode => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#33AA2255")), // Dark pink tint
+            _ => new SolidColorBrush((Color)ColorConverter.ConvertFromString("#33444444")) // Default dark gray tint
+        };
     }
 }
 
